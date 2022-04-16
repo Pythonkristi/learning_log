ad1c79b6a2b161cdc5b282897aa94e19760faa08
kristina <kris.skor.1997@mail.ru>
2022-04-16 14:19:38 +0300

    Setting

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
@@ -52,7 +52,7 @@ def new_topic(request):
 @login_required
 def new_entry(request, topic_id):
 	"""Добавляет новую запись по конкретной теме"""
-	topic = Topic.objects.get(id=topic_id)
+	topic = get_object_or_404(Topic, id=topic_id)
 	if request.method != 'POST':
 		#Данные не отправлялись; создается пустая форма
 		form = EntryForm()
